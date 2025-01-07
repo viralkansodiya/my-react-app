ee6c4992c3fcc35a96ba252e302f0519f18f86cb
ViralKansodiya <141210323+viralkansodiya@users.noreply.github.com>
2025-01-07 21:29:06 +0530

    ch

--- src/componants/Navbar.js
+++ src/componants/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link }  from 'react-router-dom';
+// import { Link }  from 'react-router-dom';
 
 
 export default function Navbar(props) {
@@ -12,20 +12,20 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg bg-${props.mode}`} style={mystyle}>
     <div className="container-fluid" >
-      <Link className="navbar-brand" to="/" style={mystyle}>{ props.title }</Link>
+      <a className="navbar-brand" href="/" style={mystyle}>{ props.title }</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent" style={mystyle}>
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/" style={mystyle}>Home</Link>
+            <a className="nav-link active" aria-current="page" href="/" style={mystyle}>Home</a>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/about" style={mystyle}>{ props.about }</Link>
+            <a className="nav-link" href="/about" style={mystyle}>{ props.about }</a>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/contact" style={mystyle}>{ props.contact }</Link>
+            <a className="nav-link" href="/contact" style={mystyle}>{ props.contact }</a>
           </li>
         </ul>
         <div className="form-check form-switch">
